f69b77fac8c5a2133c43d437fce9ba3e5663a8db
havlikmar <havlikmar@gmail.com>
2019-02-19 13:14:42 +0100

    uvodni commit

---
+++ konvertor_bakalarska_prace/src/main/java/com/github/havlikmar/konvertor_bakalarska_prace/App.java
@@ -0,0 +1,13 @@
+package com.github.havlikmar.konvertor_bakalarska_prace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
