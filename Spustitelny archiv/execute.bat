d478baaddf302fe8347dfde37993c94114ff743d
havlikmar <havlikmar@gmail.com>
2019-04-10 09:52:19 +0200

    úprava bat souboru - řešení problému se spuštěním

--- Spustitelny archiv/execute.bat
+++ Spustitelny archiv/execute.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=UTF-8 -cp "./*" com.github.havlikmar.konvertor_bakalarska_prace.main.App
+java -Dfile.encoding=UTF-8 -cp "*" com.github/havlikmar/konvertor_bakalarska_prace/main/App
